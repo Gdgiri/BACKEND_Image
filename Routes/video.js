8713206fd9ea836eb68f93da92506bb3e40abdf5
giridharan s <160309516+gdharan361@users.noreply.github.com>
2024-09-20 10:28:50 +0530

    update

--- Routes/video.js
+++ Routes/video.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createVideos, getUrlById } from "../Controllers/videoController.js";
+import { createVideos, getUrl } from "../Controllers/videoController.js";
 
 const router = express.Router();
 // POST request for creating a video
 router.post("/", createVideos);
 
 // GET request for retrieving video by ID
-router.get("/videos/:id", getUrlById);
+// router.get("/videos/:id", getUrlById);
+
+router.get("/videos", getUrl);
 export default router;
